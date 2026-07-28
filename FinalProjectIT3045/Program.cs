6f222c60d9b9dd4015c35af43b6357918cc49e0a
Aspennza <aspennza@mail.uc.edu>
2026-07-28 19:34:52 -0400

    Added and configured NSwag

--- FinalProjectIT3045/Program.cs
+++ FinalProjectIT3045/Program.cs
@@ -6,11 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddOpenApiDocument();
 
 builder.Services.AddDbContext<FinalProjectTeammatesContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("FinalProjectTeammatesContext")));
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseOpenApi();
+    app.UseSwaggerUi();
+}
+
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<FinalProjectTeammatesContext>();
