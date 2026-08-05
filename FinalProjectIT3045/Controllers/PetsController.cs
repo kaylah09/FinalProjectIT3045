a5a64f9954ad5eb476d2d2b3024941cc26056d14
Aspennza <aspennza@mail.uc.edu>
2026-08-05 09:22:44 -0400

    Modified my controllers to use better null id checks for the GET method

--- FinalProjectIT3045/Controllers/PetsController.cs
+++ FinalProjectIT3045/Controllers/PetsController.cs
@@ -17,22 +17,21 @@ public class PetsController : ControllerBase
     [HttpGet("{id?}")]
     public async Task<ActionResult> GetPet(int? id)
     {
-        if (id != null && id != 0)
-        {
-            var pet = await _context.Pets.FindAsync(id);
-
-            if (pet == null)
-            {
-                return NotFound();
-            }
-
-            return Ok(pet);
-        } else
+        if (id == 0 || id == null)
         {
             var pets = await _context.Pets.Take(5).ToListAsync();
 
             return Ok(pets);
         }
+
+        var pet = await _context.Pets.FindAsync(id);
+
+        if (pet == null)
+        {
+            return NotFound();
+        }
+
+        return Ok(pet);
     }
 
     // PUT: api/pets/5
